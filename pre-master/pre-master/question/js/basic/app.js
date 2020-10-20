3e5a37d63498199fa7e8466d2d72f27df419d6b3
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-10-20 16:14:44 +0900

    追加

--- pre-master/pre-master/question/js/basic/app.js
+++ pre-master/pre-master/question/js/basic/app.js
@@ -39,7 +39,7 @@ console.log(capitalcities)
 
 let studentlist =[
   {name:'Seedくん',from:'セブ島',hobby:'サーフィン'},
-   {name:'ErikoIchinohe',from:'青森',hobby:'旅'},,
+   {name:'ErikoIchinohe',from:'青森',hobby:'旅'},
     {name:'LuiceHeshiki',from:'ペルー',hobby:'ウクレレ'},
      {name:'YoshiyaOkumura',from:'京都',hobby:'サッカー'},
       {name:'YukoRobinson',from:'沖縄',hobby:'写真'}
@@ -48,3 +48,99 @@ let studentlist =[
 console.log(studentlist[0] ['name']);
 console.log(studentlist[3] ['from']);
 console.log(studentlist[4] ['hobby']);
+
+
+// ーーーーーーーーー条件分岐ーーーーーーーーーー
+
+/* 変数weightに任意の数字を代入してください。
+weightが200以上の場合はヘビー級
+weightが115以上、118以下の場合はバンタム級
+それ以外の場合はその他の階級
+とconsoleに表示してください。*/
+
+
+let weight=117;
+if (weight>=200) {
+ console.log('ヘビー級');
+}
+else if (weight>=115&&weight<=118) {
+	console.log('バンダム級');
+}
+else{
+	console.log('その他の階級');
+	
+}
+
+
+/*変数greetingに任意の文字列を代入してください。
+greetingがおはようの場合は日本語
+greetingがgood morningの場合は英語
+それ以外の場合は?
+とconsoleに表示してください。
+*/
+
+
+let greeting=('こんにちは')
+
+switch (greeting) {
+
+	case 'こんにちは':
+	console.log('日本語');
+	break;
+	case 'hello':
+	console.log('英語');
+ 	break;
+ 	default:
+ 	console.log('?');
+ 	break;
+}
+
+for (let i =2 ; i <= 20; i++) {
+	console.log(i)
+}
+
+for(let i=1; i<=100; i++){
+	console.log(i)
+}
+
+
+
+let studentlist2 =[
+  {name:'Seedくん',from:'セブ島',hobby:'サーフィン'},
+   {name:'ErikoIchinohe',from:'青森',hobby:'旅'},
+    {name:'LuiceHeshiki',from:'ペルー',hobby:'ウクレレ'},
+     {name:'YoshiyaOkumura',from:'京都',hobby:'サッカー'},
+      {name:'YukoRobinson',from:'沖縄',hobby:'写真'}
+];
+
+for (let i=0; i<5 ;i++){
+	console.log(studentlist2[i]['name']);
+}
+
+
+console.log('三の倍数でないものを表示する');
+for(let i = 1; i<=30; i++){
+	if (i%3===0) {
+		continue;
+	}
+	}
+
+
+for(let i=1; i<=50; i++){
+	if (i%5===0) {
+		console.log(i);
+	}else{
+		continue;
+	}
+}
+
+
+console.log('乱数を使ったプログラム')
+
+var random = Math.floor( Math.random() * 5 );
+console.log( random );
+
+
+
+let omikuji =['大吉','中吉','小吉','末吉','大凶','凶']
+console.log(omikuji[random]);
