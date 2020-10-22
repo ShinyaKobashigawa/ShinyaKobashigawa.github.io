05b0b934d6ad86ec7bb2aa3dc06de18106da9890
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-10-22 16:41:43 +0900

    追加

---
+++ jvpractice/app.js
@@ -0,0 +1,143 @@
+console.log('---------------関数------------');
+
+// 関数の書き方
+//function 関数名(){
+// 	 処理
+// }
+function myalert(){
+	// alert('自作関数からhellot');
+
+let hantei = confirm('このボタンを押してもいいですか？')
+// console.log(hantei);
+
+
+// 問題
+// okボタンが押されたら、大丈夫
+// キャンセルが押されたらやり直し
+// とコンソールに表示しよう
+
+if(hantei==true){
+	console.log('大丈夫')
+}else if (hantei==false) {
+	console.log('やり直し')
+}
+let messageSpan = document.getElementById('message')
+messageSpan.textContent = '変わりました';
+messageSpan.style.color = 'blue';
+
+if (hantei===true) 
+	{messageSpan.textContent ='大丈夫';
+}else
+    {messageSpan.textContent='やり直し';
+}
+
+}
+
+
+console.log('---------関数(引数付き)----------');
+// 関数の書き方
+// function　関数名(引数１、引数２、、、){
+// 処理	
+// }
+// 関数とは複数の処理を一つにまとめて名前を付けたもの
+// 引数とは処理を行うために必要な情報
+// alert(表示したい文字)　この場合、表示したい文字が引数に当たる
+
+function myalert2(displayMessage){
+	alert(displayMessage);
+}
+
+// 以下の関数を作成して、作成したかんすうを実行してください。
+
+//関数名:printhoge 
+//処理内容:Hogeとコンソールに表示する。
+function printhoge(msg){
+	console.log('hoge');
+}
+
+// 作った関数を実行
+printhoge();
+
+// 以下の関数を作成して、作成した関数を実行してください。
+
+// 関数名：printNum
+// 引数：　一つの数値
+// 処理内容：引数で受け取った数値をコンソールに表示する
+
+
+function printNum (num){
+	console.log(num);
+}
+printNum(80);
+
+
+// 以下の関数を作成して、作成した関数を実行してください。
+
+// 関数名：printKuku
+// 引数：　一つの数値
+// 処理内容：引数で受け取った数値の九九（１から9までかけた数)をコンソールに表示する
+// 引数に１を指定したら、1の段の計算結果がコンソールに表示されるようにする
+
+function printKuku(num){
+
+ for(let i=1;i<=9;i++){
+	console.log(num*i)
+}
+}
+
+printKuku(2)
+
+printKuku(4)
+
+
+
+// 以下の関数を作成して、作成した関数を実行してください。
+
+// 関数名：printKakezan
+// 引数：　二つの数値
+// 処理内容　引数で受け取った二つの数値の掛け算の答えをコンソールに表示する
+
+function printKakezan(num1,num2){
+	console.log(num1* num2);
+
+
+}
+
+
+
+printKakezan(200,3);
+
+
+
+
+function printIsEven(num){
+	if (num%2==0) {
+		console.log('偶数です');
+	}else{
+		console.log('奇数です');
+	}
+}
+
+printIsEven(1);
+
+
+
+function printMessage(moji,num){
+	for(let i=1;i<=num;i++)
+		console.log(moji);
+}
+
+
+printMessage('shinyua',4);
+
+
+
+
+
+
+
+
+
+
+
+
--- pre-master/pre-master/question/js/basic/app.js
+++ pre-master/pre-master/question/js/basic/app.js
@@ -1,146 +1,259 @@
-console.log('セブ島'+'留学')
-console.log('5足す８=' + String (5+8));
-console.log(4-2)
-console.log(8*2)
-console.log(6/3)
-console.log(9%2)
+// 文字列結合
+console.log('セブ島'+'留学');
 
-let country = 'Japan';
+// 四則演算
+console.log(5+8);
+console.log(5*8);
+console.log(10/2);
+console.log(5%2);
+
+// 変数
+let country='Japan';
 console.log(country);
+console.log('country');
+
+// 変数の場合’シングルクウォーテーションは付けない。
+// クウォーテーションが付いていた場合はcountryが表示される。
+
+// 再代入
 country='Philippines';
 console.log(country);
+// 
 
-let school = 'Nexseed'
-console.log(`私の所属先は、${school}です`)
-// バックスラッシュクウォーテーションを使う
+let name ='shinya';
+console.log(name);
+name='kobashigawa';
+console.log(name);
 
-let job = 'engineer'
-console.log(`私の仕事は${job}です`)
+let drink ='cola';
+console.log(drink);
+drink='fanta';
+console.log(drink);
 
-let applications = ['LINE', 'Facebook', 'Twitter'];
+// テンプレートリテラル
+let job='enjineer';
+console.log(`わたしの職業は${job}です`);
+
+let drinks ='green tea'
+console.log(`毎朝${drinks}を飲みます`);
+
+// 配列
+
+let student_list=['shinya','kobashigawa','yanshi'];
+console.log(student_list);
+console.log(student_list[2]);
+student_list.push('kobashi');
+console.log(student_list);
+
+/*変数applicationsを作成して、配列['LINE', 'Facebook', 'Twitter']を代入してください。
+変数applicationsをconsoleに表示してください。
+変数applicationsにInstagramを追加してください。
+変数applicationsの中のInstagramをconsoleに表示してください。*/
+
+let applications=['LINE','Facebook','Twitter'];
 console.log(applications);
 applications.push('Instagram');
-
 console.log(applications[3]);
 
 
-let capitalcities ={Japan:'Tokyo', Philippines:'Manila'};
-console.log(capitalcities);
-capitalcities.Amarica='Washington'
-console.log(capitalcities)
+let subject=['math','english','science'];
+console.log(subject);
+subject.push('music');
+console.log(subject[3]);
 
-// 宿題
-// クラスのみんなの名簿を二次元配列でつくってきましょう
-// 配列の中に、連想配列のかたちです
-// リストのゼロ番目の人の名前をconsoleで表示しましょう
-// リストの三番目の人の出身地を表示しましょう
-// リストの四番目の人の趣味を表示しましょう
+// 連想配列
+let user={age:25,from:'Okinawa'};
+console.log(user.age);
+console.log(user);
+
+user.hobby='soccer';
+console.log(user.hobby);
+
+/*変数capitalCitiesを作成して、連想配列{ Japan: 'Tokyo', Philippines: 'Manila' }を代入してください。
+変数capitalCitiesをconsoleに表示してください。
+変数capitalCitiesにAmerica: Washingtonを追加してください。
+変数capitalCitiesの中のWashingtonをconsoleに表示してください。*/
+
+let capitalCities={Japan:'Tokyo',Philippines:'Manila'};
+console.log(capitalCities);
+capitalCities.America='Washinton';
+console.log(capitalCities.America);
+// console.logで変数を表示する際に''をつけない。あるとそのまま表示される。
+
+let fruits={blue:'ブドウ',yellow:'バナナ'};
+console.log(fruits);
+fruits.red='アップル';
+console.log(fruits.red);
 
 
-let studentlist =[
-  {name:'Seedくん',from:'セブ島',hobby:'サーフィン'},
-   {name:'ErikoIchinohe',from:'青森',hobby:'旅'},
-    {name:'LuiceHeshiki',from:'ペルー',hobby:'ウクレレ'},
-     {name:'YoshiyaOkumura',from:'京都',hobby:'サッカー'},
-      {name:'YukoRobinson',from:'沖縄',hobby:'写真'}
-];
+// 条件分岐
+let int2 =23;
 
-console.log(studentlist[0] ['name']);
-console.log(studentlist[3] ['from']);
-console.log(studentlist[4] ['hobby']);
+if (int2>=20&&int2<=30) {
+	console.log('20歳以上です');
+}
 
-
-// ーーーーーーーーー条件分岐ーーーーーーーーーー
-
-/* 変数weightに任意の数字を代入してください。
+/*変数weightに任意の数字を代入してください。
 weightが200以上の場合はヘビー級
 weightが115以上、118以下の場合はバンタム級
 それ以外の場合はその他の階級
 とconsoleに表示してください。*/
 
-
-let weight=117;
+let weight =116;
 if (weight>=200) {
- console.log('ヘビー級');
-}
-else if (weight>=115&&weight<=118) {
+	console.log('ヘビー級');
+}else if (weight>=115&&weight<=118) {
 	console.log('バンダム級');
-}
-else{
+}else{
 	console.log('その他の階級');
-	
 }
 
+// switch
+
+let gender ='male';
+
+switch (gender){
+	case'male':
+	console.log('男性です');
+	break;
+	case'female':
+	console.log('女性です');
+	break;
+	default:
+	console.log('?');
+	break;
+}
 
 /*変数greetingに任意の文字列を代入してください。
 greetingがおはようの場合は日本語
 greetingがgood morningの場合は英語
 それ以外の場合は?
-とconsoleに表示してください。
-*/
+とconsoleに表示してください。*/
 
-
-let greeting=('こんにちは')
-
-switch (greeting) {
-
-	case 'こんにちは':
-	console.log('日本語');
+let greeting='hola';
+switch(greeting){
+	case'hello':
+	console.log('english');
 	break;
-	case 'hello':
-	console.log('英語');
- 	break;
- 	default:
- 	console.log('?');
- 	break;
+	case'hola':
+	console.log('spanish');
+	break;
+	default:
+	console.log('?');
+	break;
+
 }
 
-for (let i =2 ; i <= 20; i++) {
-	console.log(i)
+// 繰り返し
+
+/*1から10までの数字を表示するプログラム
+	変数iに1を代入する(初期化式)
+	iが10以下の間だけ繰り返す(条件式)
+	iを1回ごとに+1する(変化式)*/
+
+for (var i=1;i<=10;i++){
+	console.log(i);
 }
 
-for(let i=1; i<=100; i++){
-	console.log(i)
+// 初期化式で変数iに2を代入する
+// iが20以下の間繰り返す
+// 1回ごとにiを1増やす
+// 繰り返しのたびにiをconsoleに表示する
+
+for(let i=2;i<=20;i++){
+	console.log(i);
+}
+
+// while文
+let k=1;
+while(k<=10){
+	console.log(k);
+	(k++);
+}
+
+let y=2;
+while (y<=20){
+	console.log(y);
+	(y++);
+}
+
+// for of
+
+// 変数sportsを作成して、配列['basketball', 'soccer', 'baseball']を代入してください。
+// 変数sportsに入ってる値をfor ofを使用して全てconsoleに表示してください。
+
+
+let sports = ['basketball','soccer','baseball']
+{
+    for(let sport of sports)
+	console.log(sport);
 }
 
 
-
-let studentlist2 =[
-  {name:'Seedくん',from:'セブ島',hobby:'サーフィン'},
-   {name:'ErikoIchinohe',from:'青森',hobby:'旅'},
-    {name:'LuiceHeshiki',from:'ペルー',hobby:'ウクレレ'},
-     {name:'YoshiyaOkumura',from:'京都',hobby:'サッカー'},
-      {name:'YukoRobinson',from:'沖縄',hobby:'写真'}
-];
-
-for (let i=0; i<5 ;i++){
-	console.log(studentlist2[i]['name']);
+let foods =['rice','bread','egg']
+{
+	for (let food of foods)
+		console.log(food);
 }
 
+// for in
 
-console.log('三の倍数でないものを表示する');
-for(let i = 1; i<=30; i++){
+let prices ={
+	apple:150,
+	orange:200,
+	banana:50,
+}
+
+for (let key in prices) {
+	console.log(key+'is'+prices[key]+'peso');
+}
+
+// 変数heightsを作成して、連想配列{ skyTree : 634, tokyoTower: 333 }を代入してください。
+// 変数heightsに入ってる値をfor inを使用して全て以下のような形で、consoleに表示してください。
+// skyTreeの高さは634mです
+// tokyoTowerの高さは333mです
+
+let heights ={
+	skytree:634,
+	tokyotower:333,
+}
+
+for (let tower in heights){
+	console.log(tower+'の高さは'+heights[tower]+'mです');
+}
+
+// break
+//1 - 100までの数字を順番に足して、合計が1000を超える数値を画面に表示するプログラム
+//合計が1000を超えたあとの繰り返しは不要なため、breakで繰り返し処理を中止。
+let result = 0;
+for (let i = 1;i<=100;i++){
+	result+=i;
+	if (result>1000) {
+		console.log(`合計値が１０００を超えるのは${i}の時`)
+		break;
+	}
+}
+
+// continue
+// 1 - 30までの数値で、3の倍数ではないものを表示するプログラム
+//3の倍数の時だけ処理が不要なため、continueでスキップする。
+
+for (let i =1;i<=30;i++){
 	if (i%3===0) {
 		continue;
 	}
-	}
-
-
-for(let i=1; i<=50; i++){
-	if (i%5===0) {
-		console.log(i);
-	}else{
-		continue;
-	}
+	console.log(i);
 }
 
+// 初期化式で変数iに1を代入する
+// iが50以下の間繰り返す
+// 1回ごとにiを1増やす
+// 繰り返しのたびにiをconsoleに表示する
+// ただし、iが5の倍数でない場合は表示しない。
 
-console.log('乱数を使ったプログラム')
-
-var random = Math.floor( Math.random() * 5 );
-console.log( random );
-
-
-
-let omikuji =['大吉','中吉','小吉','末吉','大凶','凶']
-console.log(omikuji[random]);
+for (let i = 1 ; i<=50 ; i++){
+	if (i%5!==0) {
+		continue;
+	}
+	console.log(i);
+}
