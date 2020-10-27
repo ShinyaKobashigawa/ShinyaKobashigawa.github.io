9ba5251af698ed4e153ee9bc0f40fe26d567c615
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-10-28 00:01:26 +0900

    15以外の宿題

--- 1027homework.js/app2.js
+++ 1027homework.js/app2.js
@@ -46,12 +46,14 @@
 
 
 // Q 6
-var text =document.querySelector('#q6-text');
-text.addEventListener('click',function(){
-  this.style.color = 'red';
-});
-
-
+// var text =document.querySelector('#q6-text');
+// text.addEventListener('click',function(){
+//   this.style.color = 'red';
+// });
+function changeColor(){
+  document.querySelector('#q6-text').style.color='green';
+};
+ changeColor()
 
 // Q7
 
@@ -78,7 +80,7 @@ text3.addEventListener('mouseleave',function(){
 
 
 // Q 9
-
+// 自分の
 var input_q9 = document.querySelector('#q9-input');
 var result_q9 = document.querySelector('#q-9result')
 function submit(){
@@ -86,12 +88,39 @@ function submit(){
   result_q9.textContent = input_q9.value;
 }
 
+// //このみんさん 
+// let q9 = document.querySelector('#q9-btn');
+//   q9.addEventListener('click',function(){
+//   let input =document.querySelector('#q9-input');
+//   let result =document.querySelector('#q9-result');
+//   result.textContent = input.value;
+
+// // megさん
+// var input = document.querySelector("#q9-input");
+//  var text = document.querySelector("#q9-result");
+//  var btn = document.querySelector('#q9-btn');
+// console.log(btn);
+// btn.addEventListener('click', function () {
+//   text.textContent = input.value;
+
+// // るいすさん
+// var msg = document.querySelector('#q9-input')
+// var msg1 = document.querySelector('#q9-result')
+// var btn = document.querySelector('#q9-btn')
+// console.log(msg)
+// console.log(msg1)
+// console.log(btn)
+// btn.addEventListener('click',function(){msg1.textContent = msg.value})
+
 // Q 10
 var input = document.querySelector('#q10-input');
 var result = document.querySelector('#q10-result');
   function submit2(){
      if (input.value<20) {
       result.textContent = ('20歳未満の方の利用は禁止です。');
+     }
+     else if(input.value>60){
+      result.textContent = ('６０歳以上の方は、半額です');
      }   
      else{
       result.textContent=input.value;
@@ -101,11 +130,13 @@ var result = document.querySelector('#q10-result');
 
 
 // Q 11
+
 var input_name = document.querySelector('#q11-input-name');
 var result_name = document.querySelector('#q11-result-name')
 var input_age = document.querySelector('#q11-input-age');
 var result_age = document.querySelector('#q11-result-age');
-  function submit3(){
+var button_11 = document.querySelector('.q11-btn');
+ button_11.addEventListener('click',function(){
   if (input_name.value=='') {
        result_name.textContent = ('名前が空欄です');
        result_name.style.color='red';
@@ -120,26 +151,59 @@ var result_age = document.querySelector('#q11-result-age');
   else{
        result_age.textContent=input_age.value;
   }
-}
+ })
 
 
 // Q 12
-
+var parent_text = document.querySelector('.q12-result');
+var btn_12 = document.querySelector('.q12-btn');
+ btn_12.addEventListener('click',function(){
+  var child_p = document.createElement('p');
+  child_p.textContent = 'Google';
+   parent_text.appendChild(child_p);
+ })
 
 // Q 13
-// 飛ばしていいです。
-
+var addition = document.querySelector('.q13-btn');
+var parent_box = document.querySelector('#q13-box');
+var melon = document.querySelector('.melon');
+addition.addEventListener('click',function(){
+  var child_li = document.createElement('li');
+  child_li.textContent = 'リンゴ';
+  parent_box.insertBefore(child_li,melon)
+})
 
 // Q 14
 var del = document.querySelector('#chicken');
-function submit6(){
+var but = document.querySelector('.q14-btn');
+but.addEventListener('click',function(){
   del.remove();
-}
+})
 
 // Q 15 (難)
 // switchまたはifを使います。
 // 要素.href = 'https://google.com';
 // でリンク先を追加できます。
 
+var web_list = document.querySelector('q15-link-list');
+var parent_text2 = document.querySelector('q15-btn');
 
- 
+var child_a = document.createElement('a');
+child_a.textContent ='google';
+child_a.href ='https://google.com';
+
+var child_a2 = document.createElement('a');
+child_a2.textContent = 'youtuve';
+child_a2.href ='https://youtube.com';
+
+var child_a3 = document.createElement('a');
+child_a3.textContent = 'amazon';
+child_a3.href ='https://amazon.com';
+
+parent_text2.addEventListener('click',function(){
+  for (var i = 1;i<3;i++){
+
+    }
+  
+  parent_text2.appendChild(child_a);
+})
