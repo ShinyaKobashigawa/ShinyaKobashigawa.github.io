f14ad1dd931561d3f0064d851f9aa812bcb22291
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-07 19:40:45 +0900

    練習

--- ToDoList/js/todo.js
+++ ToDoList/js/todo.js
@@ -1,36 +1,41 @@
-let data = ['shinuya','dog','cat'];
-
- let stop_input = document.querySelector('.input');
-for(let i of data){
- Genkidama(i);
+let Dragonball = ['goku','kuririn','pikkoro']
+// console.log(Dragonball[0])
+// genkidama(Dragonball[0])
+// for(let i = 0; i < Dragonball.length ; i++){
+//     genkidama(Dragonball[i]);
+// }
+for(let i of Dragonball){
+    genkidama(Dragonball[i]);
 }
-Genkidama(data[0])
+let stop_input = document.querySelector('.input');
+
 
 let button = document.querySelector('.btn');
 button.addEventListener('click',function(){
      if (stop_input.value != '') {
-     Genkidama(stop_input.value)
-        }
+       genkidama(stop_input.value);
+     }
 })
 
 
-      function Genkidama(kamesenninn){          
-                   
-                    let stop_list = document.querySelector('.stop-list');
-                    let list = document.createElement('li');
-                    list.classList.add('list');
-                    list.textContent = kamesenninn;
-                    stop_list.appendChild(list);
-                    let stop_i = document.createElement('i')
-                    stop_i.classList.add('fas');
-                    stop_i.classList.add('fa-check');
-                    list.appendChild(stop_i);
-                    stop_i.addEventListener('click',function(){
-                    let judge = confirm('Are you sure you want to remove this list?');
-                    if (judge === true){
-                        list.remove();
-                    }
-                    })
-                    }
 
-                 
+
+function genkidama(kamesennin){
+                let stop_list = document.querySelector('.stop-list');
+                let list = document.createElement('li');
+                list.classList.add('list');
+                list.textContent = kamesennin;
+                stop_list.appendChild(list);
+                let stop_i = document.createElement('i')
+                stop_i.classList.add('fas');
+                stop_i.classList.add('fa-check');
+                list.appendChild(stop_i);
+                stop_i.addEventListener('click',function(){
+                let judge = confirm('Are you sure you want to remove this list?');
+                if (judge === true){
+                    list.remove();
+                }
+                })
+                }
+
+         
