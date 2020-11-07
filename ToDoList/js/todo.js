33b2246ab6120796e66842b4eef700349452ad8d
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-07 19:09:35 +0900

    ローカルストレージ追加前

--- ToDoList/js/todo.js
+++ ToDoList/js/todo.js
@@ -1,26 +1,36 @@
+let data = ['shinuya','dog','cat'];
+
+ let stop_input = document.querySelector('.input');
+for(let i of data){
+ Genkidama(i);
+}
+Genkidama(data[0])
 
 let button = document.querySelector('.btn');
 button.addEventListener('click',function(){
-    let stop_input = document.querySelector('.input');
-    if (stop_input.value != '') {
-	let stop_list = document.querySelector('.stop-list');
-	let list = document.createElement('li');
-	list.classList.add('list');
-	list.textContent = stop_input.value;
-    stop_list.appendChild(list);
-    // let stop_div =document.createElement('div');
-    // stop_div.classList.add('delete');
-    // stop_div.textContent = 'Delete';
-    // list.appendChild(stop_div);
-    let stop_i = document.createElement('i')
-    stop_i.classList.add('fas');
-    stop_i.classList.add('fa-check');
-    list.appendChild(stop_i);
-    stop_i.addEventListener('click',function(){
-    let judge = confirm('Are you sure you want to remove this list?');
-    if (judge === true){
-    	list.remove();
-    }
-    })
-    }
+     if (stop_input.value != '') {
+     Genkidama(stop_input.value)
+        }
 })
+
+
+      function Genkidama(kamesenninn){          
+                   
+                    let stop_list = document.querySelector('.stop-list');
+                    let list = document.createElement('li');
+                    list.classList.add('list');
+                    list.textContent = kamesenninn;
+                    stop_list.appendChild(list);
+                    let stop_i = document.createElement('i')
+                    stop_i.classList.add('fas');
+                    stop_i.classList.add('fa-check');
+                    list.appendChild(stop_i);
+                    stop_i.addEventListener('click',function(){
+                    let judge = confirm('Are you sure you want to remove this list?');
+                    if (judge === true){
+                        list.remove();
+                    }
+                    })
+                    }
+
+                 
