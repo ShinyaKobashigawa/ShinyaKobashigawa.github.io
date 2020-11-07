4b776693c378ec4be7a17aa8ec478dfe74e9e63b
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-07 20:58:02 +0900

    ハンバーガーメニューを出した

---
+++ HP(kurashiru)/kurashiru.js
@@ -0,0 +1,11 @@
+$(function(){
+// let shinya = $('.page-title');
+// $(shinya).css('opacity','0.1');
+$('.hamburger').on('click',function(){
+ $(this).toggleClass('on');
+})
+
+
+
+
+})
--- ToDoList/js/todo.js
+++ ToDoList/js/todo.js
@@ -1,41 +1,35 @@
-let Dragonball = ['goku','kuririn','pikkoro']
-// console.log(Dragonball[0])
-// genkidama(Dragonball[0])
-// for(let i = 0; i < Dragonball.length ; i++){
-//     genkidama(Dragonball[i]);
+let dragonball = ['goku','kuririn','pikkoro'];
+// genkidama(dragonball[0]);
+// for(let i = 0; i <dragonball.length; i++){
+//     genkidama(dragonball[i]);
 // }
-for(let i of Dragonball){
-    genkidama(Dragonball[i]);
+for(let i  of dragonball){
+    genkidama(i);
 }
+
 let stop_input = document.querySelector('.input');
-
-
 let button = document.querySelector('.btn');
 button.addEventListener('click',function(){
      if (stop_input.value != '') {
-       genkidama(stop_input.value);
-     }
+    genkidama(stop_input.value)
+}
 })
 
-
-
-
 function genkidama(kamesennin){
-                let stop_list = document.querySelector('.stop-list');
-                let list = document.createElement('li');
-                list.classList.add('list');
-                list.textContent = kamesennin;
-                stop_list.appendChild(list);
-                let stop_i = document.createElement('i')
-                stop_i.classList.add('fas');
-                stop_i.classList.add('fa-check');
-                list.appendChild(stop_i);
-                stop_i.addEventListener('click',function(){
-                let judge = confirm('Are you sure you want to remove this list?');
-                if (judge === true){
-                    list.remove();
-                }
-                })
-                }
-
-         
+                    let stop_list = document.querySelector('.stop-list');
+                    let list = document.createElement('li');
+                    list.classList.add('list');
+                    list.textContent = kamesennin;
+                    stop_list.appendChild(list);
+                    let stop_i = document.createElement('i')
+                    stop_i.classList.add('fas');
+                    stop_i.classList.add('fa-check');
+                    list.appendChild(stop_i);
+                    stop_i.addEventListener('click',function(){
+                    let judge = confirm('Are you sure you want to remove this list?');
+                    if (judge === true){
+                        list.remove();
+                    }
+                    })
+                    }
+                    
