f209e0aa4b7331dbd73d41e6fb280249fc11ffb4
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-11 01:03:23 +0900

    init

---
+++ 和歌山/wakayama.js
@@ -0,0 +1,15 @@
+$(function(){
+    // let topBtn = $('.page-top');
+    // topBtn.hide();
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > 500){
+    //         topBtn.fadeIn();
+    //     } else {
+    //         topBtn.fadeOut();
+    //     }
+    // });
+    $('.page-top').on('click',function(){
+    $('body, html').animate({scrollTop:0},500);
+    });
+});
+
